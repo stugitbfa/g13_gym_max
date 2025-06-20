b13154fdb2ac7fe72339bdc33ec297b83c708302
Subham Singh <srajivranjan12@gmail.com>
2025-06-20 11:27:55 +0530

    Delete apps/web directory

--- apps/web/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path('', views.enter_email, name='enter_email'),  # Home page is now email input
-    path('verify-otp/', views.verify_otp, name='verify_otp'),  # This should already exist
-    path('login/', views.login_view, name='login'),  # Existing login view
-]
